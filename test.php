e895139bc10fca610028459291e3701cb4550c1c
unknown <fkicheon@gmail.com>
2018-03-03 10:22:49 +0900

    test commit

---
+++ test.php
@@ -0,0 +1,72 @@
+ <script type="text/javascript">
+  //<![CDATA[
+  (function(){
+    var a = function() {try{return !!window.addEventListener} catch(e) {return !1} },
+    b = function(b, c) {a() ? document.addEventListener("DOMContentLoaded", b, c) : document.attachEvent("onreadystate                                         change", b)};
+    b(function(){
+      var a = document.getElementById('cf-content');a.style.display = 'block';
+      setTimeout(function(){
+        var s,t,o,p,b,r,e,a,k,i,n,g,f, aoDZILV={"jpdSj":+((!+[]+!![]+[])+(+!![]))};
+        alert('val1='+aoDZILV.jpdSj)
+
+        t = document.createElement('div');
+        t.innerHTML="<a href='/'>x</a>";
+        t = t.firstChild.href;r = t.match(/https?:\/\//)[0];
+        t = t.substr(r.length); t = t.substr(0,t.length-1);
+        a = document.getElementById('jschl-answer');
+        f = document.getElementById('challenge-form');
+        ;
+        
+        aoDZILV.jpdSj-=+((+!![]+[])+(!+[]+!![]+!![]+!![]+!![]+!![]));
+        alert('val2='+aoDZILV.jpdSj)
+        aoDZILV.jpdSj*=+((!+[]+!![]+!![]+[])+(+[]));
+        alert('val3='+aoDZILV.jpdSj)
+        aoDZILV.jpdSj-=+((!+[]+!![]+!![]+!![]+[])+(!+[]+!![]+!![]));
+        alert('val4='+aoDZILV.jpdSj)
+        aoDZILV.jpdSj+=+((+!![]+[])+(!+[]+!![]+!![]));
+        alert('val5='+aoDZILV.jpdSj)
+        aoDZILV.jpdSj*=+((!+[]+!![]+[])+(!+[]+!![]+!![]+!![]+!![]+!![]));
+        alert('val6='+aoDZILV.jpdSj)
+        aoDZILV.jpdSj*=+((!+[]+!![]+!![]+!![]+[])+(!+[]+!![]+!![]+!![]+!![]+!![]+!![]+!![]));
+        alert('val7='+aoDZILV.jpdSj)
+        aoDZILV.jpdSj*=+((+!![]+[])+(+!![]));
+        alert('val8='+aoDZILV.jpdSj)
+        aoDZILV.jpdSj*=+((!+[]+!![]+[])+(!+[]+!![]+!![]+!![]+!![]+!![]+!![]+!![]));
+        alert('val9='+aoDZILV.jpdSj+'==t='+t)
+        a.value = parseInt(aoDZILV.jpdSj, 10) + t.length;
+        alert('val10='+aoDZILV.jpdSj)
+        '; 121'
+        f.action += location.hash;
+        //f.submit();
+      }, 0);
+    }, false);
+  })();
+  //]]>
+</script>
+
+<body marginheight="0" marginwidth="0">
+
+<div class="cloudflare_box">
+<div class="cf-browser-verification cf-im-under-attack">
+  <noscript><h1 data-translate="turn_on_js" style="color:#bd2426;">Please turn JavaScript on and reload the page.</h1>  
+  </noscript>
+  <div id="cf-content" style="display:none">
+
+    <div>
+      <div class="bubbles"></div>
+      <div class="bubbles"></div>
+      <div class="bubbles"></div>
+    </div>
+    <h1><span data-translate="checking_browser">Checking your browser before accessing</span> coinone.co.kr.</h1>
+
+    <p data-translate="process_is_automatic">This process is automatic. Your browser will redirect to your requested c                                         ontent shortly.</p>
+    <p data-translate="allow_5_secs">Please allow up to 5 seconds&hellip;</p>
+  </div>
+
+  <form id="challenge-form" action="/cdn-cgi/l/chk_jschl" method="get">
+    <input type="hidden" name="jschl_vc" value="9090c26a80657eaff5af690a171e8a3f"/>
+    <input type="hidden" name="pass" value="1519972842.486-eSWBUoMEPs"/>
+    <input type="hidden" id="jschl-answer" name="jschl_answer"/>
+  </form>
+</div>
+</div>
